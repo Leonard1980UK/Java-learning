b89616db3de52b60ef3002633ce534d12ded818a
LeonardUK1980 <nema.otto@gmailcom>
2018-01-24 20:01:14 +0000

    Add main method

--- Substitute/src/Substitute.java
+++ Substitute/src/Substitute.java
@@ -3,4 +3,16 @@ import java.lang.*;
 
 public class Substitute {
 
+    public static void main(String[] args) {
+        // read key and code
+        Scanner sc = new Scanner(System.in);
+        String key = sc.nextLine();
+        String code = sc.nextLine();
+        sc.close();
+
+        // create an instance of the class for using getValue method
+        Substitute substitute = new Substitute();
+
+        substitute.getValue(key,code);
+    }
 }
