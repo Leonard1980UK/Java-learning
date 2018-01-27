5bf77d12edc7ac59d617b7df370ecaca6e3c4345
LeonardUK1980 <nema.otto@gmail.com>
2018-01-27 18:17:01 +0000

    rename project

--- Print Linked List elements/src/LinkedLIst.java
+++ Print Linked List elements/src/LinkedLIst.java
@@ -1,5 +1,6 @@
 import java.lang.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class LinkedLIst {
 
@@ -32,25 +33,34 @@ public class LinkedLIst {
         }
     }
 
-    void add(LinkedLIst llist, int data){
+    void add(LinkedLIst llist, int uData){
 
         if (head == null){
-            llist.head = new Node(data);
+            llist.head = new Node(uData);
         }else{
-            Node n = head;
-            while (n.next != null){
-
+            Node node = head;
+            while (node.next != null){
+                node = node.next;
             }
+            node.next = new Node(uData);
 
         }
-
-
     }
+
+
 
     public static void main(String[] args)
     {
         // instantiate a linked list class
         LinkedLIst llist = new LinkedLIst();
+        for (int i=0; i<5;i++) {
+            System.out.println("Add an integer to add our Linked List");
+            Scanner sc = new Scanner(System.in);
+            llist.add(llist, sc.nextInt());
+        }
+
+        llist.addBefore(llist, 3, 888);
+        llist.printList(llist.head);
 
 
     }
