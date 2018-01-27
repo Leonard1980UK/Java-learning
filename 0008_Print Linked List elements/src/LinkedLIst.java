14bbb8a4381d8055b28a91ec62429740ac658b85
LeonardUK1980 <nema.otto@gmail.com>
2018-01-27 22:02:06 +0000

    add addAfter method

--- 0008_Print Linked List elements/src/LinkedLIst.java
+++ 0008_Print Linked List elements/src/LinkedLIst.java
@@ -19,7 +19,6 @@ public class LinkedLIst {
         }
     }
 
-
     void printList(Node head)
     {
         if (head != null){
@@ -32,9 +31,9 @@ public class LinkedLIst {
             }
         }
     }
-
+    // add element at the end
     void add(LinkedLIst llist, int uData){
-
+        // if there is no element in list yet
         if (head == null){
             llist.head = new Node(uData);
         }else{
@@ -46,20 +45,45 @@ public class LinkedLIst {
 
         }
     }
+    // add element after Nth place
+    void addAffter(int index, int uData){
 
+        Node newNode = new Node(uData);
+        int counter = 1;
+        Node node = head;
+        while (counter < index && node != null){
+            node = node.next;
+            counter++;
+        }
+        if (node == null){
+            System.out.println("Given index doesn't exist");
+            return;
+        }
+        newNode.next = node.next;
+        node.next = newNode;
 
+    }
 
     public static void main(String[] args)
     {
+
         // instantiate a linked list class
         LinkedLIst llist = new LinkedLIst();
-        for (int i=0; i<5;i++) {
-            System.out.println("Add an integer to add our Linked List");
-            Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many integer do you want to put in?");
+        Scanner sc = new Scanner(System.in);
+        int nrOfElem = sc.nextInt();
+        System.out.println("Add an integer to add our Linked List");
+
+        for (int i = 0; i < nrOfElem;i++) {
             llist.add(llist, sc.nextInt());
         }
+        System.out.println("After which element, you would like to add the new element");
+        int index = sc.nextInt();
+        System.out.println("Please add new element");
+        int data = sc.nextInt();
 
-//        llist.addBefore(llist, 3, 888);
+        llist.addAffter(index, data);
         llist.printList(llist.head);
 
 
