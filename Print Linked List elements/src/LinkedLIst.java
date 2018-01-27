c005e712a032ed2159f2d803f7dbecd586f34195
LeonardUK1980 <nema.otto@gmail.com>
2018-01-27 16:36:19 +0000

    add list method

--- Print Linked List elements/src/LinkedLIst.java
+++ Print Linked List elements/src/LinkedLIst.java
@@ -3,5 +3,42 @@ import java.io.*;
 
 public class LinkedLIst {
 
+    // declare head node with data 0
+    private Node head;
 
+    // node class
+    class Node {
+        int data;
+        Node next;
+
+        //constructor
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+
+    void printList(Node head)
+    {
+        if (head != null){
+            System.out.print(head.data);
+            Node n = head;
+            while (n.next != null){
+                n = n.next;
+                System.out.print(" ");
+                System.out.print(n.data);
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        // instantiate a linked list class
+        LinkedLIst llist = new LinkedLIst();
+
+
+    }
 }
