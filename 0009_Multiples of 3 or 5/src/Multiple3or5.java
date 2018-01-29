a95e4847d2a8e73c5c25fe987ba13258d341f62f
LeonardUK1980 <nema.otto@gmail.com>
2018-01-29 23:35:10 +0000

    Find a solution

---
+++ 0009_Multiples of 3 or 5/src/Multiple3or5.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Multiple3or5 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++){
+            if (i%3 == 0){
+                sum+=i;
+            }
+        }
+
+    }
+}
