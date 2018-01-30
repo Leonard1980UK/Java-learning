dbbe47f68d6ed7f95a2081830ef307edad4e7fa2
LeonardUK1980 <nema.otto@gmail.com>
2018-01-30 00:57:14 +0000

    Solution

--- 0009_Multiples of 3 or 5/src/Multiple3or5.java
+++ 0009_Multiples of 3 or 5/src/Multiple3or5.java
@@ -1,16 +1,20 @@
 import java.lang.*;
-import java.io.*;
+
 import java.util.*;
 
 public class Multiple3or5 {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
         int sum = 0;
-        for (int i = 0; i < 10; i++){
-            if (i%3 == 0){
+        for (int i = 1; i <= count; i++){
+            if (i%5 == 0 || i%3 == 0 ){
                 sum+=i;
             }
         }
+
+        System.out.println(sum);
 
     }
 }
