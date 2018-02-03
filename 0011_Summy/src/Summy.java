d01a5912d655db947e36175c50f1db0954ce7107
LeonardUK1980 <nema.otto@gmail.com>
2018-02-03 10:26:27 +0000

    Create solution

--- 0011_Summy/src/Summy.java
+++ 0011_Summy/src/Summy.java
@@ -1,17 +1,13 @@
 import java.lang.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Summy {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int sum = 0;
-        for (int i = 0; i < input.length(); i++){
-            int num = Character.getNumericValue(input.charAt(i));
-            if (num > 0)
-                sum += num;
-        }
-        System.out.println(sum);
+        String[] input = sc.nextLine().split(" ");
+
+        System.out.println(Stream.of(input).mapToLong(Long::valueOf).sum());
     }
 }
