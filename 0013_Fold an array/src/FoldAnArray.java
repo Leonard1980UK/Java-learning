e8dee31b464c1506a8f8be4439e10d8e4f902710
LeonardUK1980 <nema.otto@gmail.com>
2018-02-08 23:49:28 +0000

    Prepare problem

---
+++ 0013_Fold an array/src/FoldAnArray.java
@@ -0,0 +1,18 @@
+import java.util.stream.Stream;
+
+public class FoldAnArray {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5};
+        int oldArrLength =  array.length;
+        int half = (array.length / 2) + 1;
+        int[] newArryLength = new int[] {half};
+
+
+        for (int i = 0; i < half; i++){
+
+        }
+    }
+
+}
